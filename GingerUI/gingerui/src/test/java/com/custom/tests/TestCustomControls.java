233afec320b9c6be5ffefb6449760584497c0cc3
Nagesh Phaniraj <nageshphaniraj81@gmail.com>
2017-06-06 20:45:09 +0530

    added custom test package

---
+++ GingerUI/gingerui/src/test/java/com/custom/tests/TestCustomControls.java
@@ -0,0 +1,24 @@
+package com.custom.tests;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class TestCustomControls {
+
+	@BeforeClass
+	public void beforeClass() {
+		// code will be added soon
+	}
+
+	@Test
+	public void f() {
+		// code will be added soon
+	}
+
+	@AfterClass
+	public void afterClass() {
+		// code will be added soon
+	}
+
+}
